b9d3e7d4904712a7e1f333d5a94e9af4f95fdf33
8YBY8sd <b9yu@ucsd.edu>
2023-05-14 13:39:43 -0700

    finished part1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,13 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  //https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+  if(localStorage.getItem('recipes') == null){
+    return [];
+  }
+  else{ 
+    return JSON.parse(localStorage.getItem('recipes'));
+  }
 }
 
 /**
@@ -35,10 +42,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  let mainELe = document.querySelector('main');
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for (let i = 0; i < recipes.length; i++){
+    let recipeCardEle = document.createElement("recipe-card");
+    recipeCardEle.data = recipes[i];
+    mainELe.append(recipeCardEle);
+  }
 }
 
 /**
